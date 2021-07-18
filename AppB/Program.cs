9015fcf66e8bc50af96ad22f66a323aa92760811
Joanna Zheng <v-joazh@microsoft.com>
2021-07-18 05:00:39 -0700

    Add project files.

---
+++ AppB/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AppB
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AppC/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AppC
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
